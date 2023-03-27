c13f30ccb316453d383dbfe1ec36313d526f716d
Nahiyan3 <Nahiyan669@gmail.com>
2023-03-27 09:44:21 +0600

    Add project files.

---
+++ Task10/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    internal class Book
+    {
+      
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int CopiesAvailable { get; set; }
+
+
+        public Book(string iSBN, string title, string author, string genre, int copiesAvailable)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            CopiesAvailable = copiesAvailable;
+        }
+
+
+    }
+}
---
+++ Task10/Borrower.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    internal class Borrower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
+        public List<Book> BorrowedBooks = new List<Book>();
+        public decimal Fine { get; set; } = 0;
+        
+        public Book Book { get; set; }
+        public Borrower(int id, string name, string contactInfo)
+        {
+            Id = id;
+            Name = name;
+            ContactInfo = contactInfo;
+        }
+
+        public int MaxBooksAllowed { get; set; } = 3;
+
+        public void  BorrowBook(Book book)
+        {
+            if(BorrowedBooks.Count >= MaxBooksAllowed)
+            {
+                throw new InvalidOperationException("You can not borrow more than 3 books at a time");
+            }
+
+            if(Book.CopiesAvailable == 0)
+            {
+                throw new InvalidOperationException("The book is not available");
+            }
+
+        }
+    }
+
+    
+    }
+
